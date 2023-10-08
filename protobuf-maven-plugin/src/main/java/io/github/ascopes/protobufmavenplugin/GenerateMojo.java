173df45c059b7510475a50bc4bdd235130f8a9ce
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2023-10-08 12:18:19 +0100

    Initial project structure

---
+++ protobuf-maven-plugin/src/main/java/io/github/ascopes/protobufmavenplugin/GenerateMojo.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2023, Ashley Scopes.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ascopes.protobufmavenplugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+
+/**
+ * Generate Java classes from Protobuf sources.
+ *
+ * @author Ashley Scopes
+ */
+@Mojo(
+    name = "generate",
+    defaultPhase = LifecyclePhase.GENERATE_SOURCES,
+    threadSafe = true
+)
+public class GenerateMojo extends AbstractMojo {
+
+  @Override
+  public void execute() {
+    getLog().info("Hello, World!");
+  }
+}
