125c22883212fb7ba3a5a519f77c831bdaf41542
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2023-12-27 20:42:11 +0000

    Add example for Salesforce custom plugin (closes GH-47)

---
+++ src/it/java-reactor-grpc-simple/src/main/java/org/example/helloworld/ReactorGreetingServiceImpl.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2023, Ashley Scopes.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.example.helloworld;
+
+import reactor.core.publisher.Mono;
+
+public class ReactorGreetingServiceImpl extends ReactorGreetingServiceGrpc.GreetingServiceImplBase {
+  @Override
+  public Mono<GreetingResponse> greet(Mono<GreetingRequest> request) {
+    return request.map(body -> GreetingResponse.newBuilder()
+            .setText("Hello, " + body.getName() + "!")
+            .build());
+  }
+}
