19a0ec28103ec1cedeb350d0ad0eff2b4984eee2
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2024-02-17 14:43:27 +0000

    Add missing tests for FileUtils

--- src/test/java/io/github/ascopes/protobufmavenplugin/platform/FileUtilsTest.java
+++ src/test/java/io/github/ascopes/protobufmavenplugin/platform/FileUtilsTest.java
@@ -19,10 +19,13 @@ import static io.github.ascopes.protobufmavenplugin.fixtures.RandomFixtures.some
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assumptions.assumeThat;
 
+import io.github.ascopes.protobufmavenplugin.fixtures.TestFileSystem;
+import java.io.IOException;
 import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-
 import org.jspecify.annotations.Nullable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -100,7 +103,6 @@ class FileUtilsTest {
       "      '',      ",
   })
   @ParameterizedTest(name = ".getFileExtension(\".../{0}\") returns \"{1}\"")
-  @SuppressWarnings({"OptionalUsedAsFieldOrParameterType", "AssertBetweenInconvertibleTypes"})
   void getFileExtensionReturnsTheExpectedValue(
       String fileName,
       @Nullable String expected,
@@ -144,5 +146,84 @@ class FileUtilsTest {
     assertThatThrownBy(() -> FileUtils.getFileSystemProvider(scheme))
         .isInstanceOf(FileSystemNotFoundException.class)
         .hasMessage("No file system provider for %s was found", scheme);
+  }
+
+  @DisplayName(".makeExecutable makes the path executable when supported")
+  @Test
+  void makeExecutableMakesThePathExecutableWhenSupported() throws IOException {
+    // Given
+    try (var tempFs = TestFileSystem.linux()) {
+      var file = tempFs.givenFileExists("foo", "bar", "baz");
+
+      assumeThat(Files.isExecutable(file)).isFalse();
+
+      // When
+      FileUtils.makeExecutable(file);
+
+      // Then
+      assertThat(file)
+          .isRegularFile()
+          .isExecutable();
+    }
+  }
+
+  @DisplayName(".makeExecutable exits silently when not supported")
+  @Test
+  void makeExecutableMakesThePathExecutableWhenNotSupported() throws IOException {
+    // Given
+    try (var tempFs = TestFileSystem.windows()) {
+      var file = tempFs.givenFileExists("foo", "bar", "baz");
+
+      // When
+      FileUtils.makeExecutable(file);
+
+      // Then
+      assertThat(file)
+          .isRegularFile()
+          .isExecutable();
+    }
+  }
+
+  @DisplayName(".changeRelativePath produces the expected result on the same file system")
+  @Test
+  void changeRelativePathProducesTheExpectedResultOnTheSameFileSystem(
+      @TempDir Path tempDir
+  ) throws IOException {
+    // Given
+    var dir1 = tempDir.resolve("foo").resolve("bar").resolve("baz");
+    var dir2 = tempDir.resolve("do").resolve("ray").resolve("me");
+    var existingPath = dir1.resolve("some").resolve("file.txt");
+    Files.createDirectories(dir1);
+    Files.createDirectories(dir2);
+
+    // When
+    var actualPath = FileUtils.changeRelativePath(dir2, dir1, existingPath);
+
+    // Then
+    assertThat(actualPath)
+        .isEqualTo(dir2.resolve("some").resolve("file.txt"));
+  }
+
+  @DisplayName(".changeRelativePath produces the expected result across file systems")
+  @Test
+  void changeRelativePathProducesTheExpectedResultAcrossFileSystems() throws IOException {
+    // Given
+    try (
+        var fs1 = TestFileSystem.linux();
+        var fs2 = TestFileSystem.windows()
+    ) {
+      var dir1 = fs1.getRoot().resolve("foo").resolve("bar").resolve("baz");
+      var dir2 = fs2.getRoot().resolve("do").resolve("ray").resolve("me");
+      var existingPath = dir1.resolve("some").resolve("file.txt");
+      Files.createDirectories(dir1);
+      Files.createDirectories(dir2);
+
+      // When
+      var actualPath = FileUtils.changeRelativePath(dir2, dir1, existingPath);
+
+      // Then
+      assertThat(actualPath)
+          .isEqualTo(dir2.resolve("some").resolve("file.txt"));
+    }
   }
 }
