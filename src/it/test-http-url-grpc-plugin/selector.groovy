200ed904935a281f72bb14b9c3b4a602d8935d7a
ascopes <73482956+ascopes@users.noreply.github.com>
2024-02-17 18:30:47 +0000

    Add missing selector.groovy for URL GRPC test

---
+++ src/it/test-http-url-grpc-plugin/selector.groovy
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2023 - 2024, Ashley Scopes.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Only execute on x86 Linux systems, as the URL we test against is CPU and OS specific.
+if (System.getProperty("os.name").equalsIgnoreCase("Linux")) {
+  if (System.getProperty("os.arch").equalsIgnoreCase("amd64")) {
+    println("This system is Linux and x86_64, running this test case...")
+    return true
+  } else {
+    println("Skipping this test case as the system is not x86_64")
+    return false
+  }
+} else {
+  println("Not running this test as this is not a Linux system...")
+  return false
+}
