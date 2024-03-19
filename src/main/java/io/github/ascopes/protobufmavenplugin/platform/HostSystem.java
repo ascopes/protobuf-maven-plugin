66947ba72070800d57f9b3f67ee1c7f1da63e79d
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2024-03-19 18:09:39 +0000

    Remove unused imports in HostSystem.java

--- src/main/java/io/github/ascopes/protobufmavenplugin/platform/HostSystem.java
+++ src/main/java/io/github/ascopes/protobufmavenplugin/platform/HostSystem.java
@@ -21,7 +21,6 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
