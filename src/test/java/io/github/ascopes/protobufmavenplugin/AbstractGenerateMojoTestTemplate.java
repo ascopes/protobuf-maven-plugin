c5c1e545f683408578192fe9a58eff23204094ca
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2024-04-12 16:07:54 +0100

    Add base tests for AbstractGenerateMojo derivatives

---
+++ src/test/java/io/github/ascopes/protobufmavenplugin/AbstractGenerateMojoTestTemplate.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2023 - 2024, Ashley Scopes.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ascopes.protobufmavenplugin;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import io.github.ascopes.protobufmavenplugin.generate.SourceRootRegistrar;
+import java.nio.file.Path;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Build;
+import org.apache.maven.project.MavenProject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+abstract class AbstractGenerateMojoTestTemplate<A extends AbstractGenerateMojo> {
+
+  abstract A getInstance();
+
+  abstract SourceRootRegistrar expectedSourceRootRegistrar();
+
+  abstract Path expectedDefaultSourceDirectory(MavenSession session);
+
+  abstract Path expectedDefaultOutputDirectory(MavenSession session);
+
+  @DisplayName("the sourceRootRegistrar is the expected value")
+  @Test
+  void sourceRootRegistrarIsTheExpectedValue() {
+    // Then
+    assertThat(getInstance().sourceRootRegistrar())
+        .isEqualTo(expectedSourceRootRegistrar());
+  }
+
+  @DisplayName("the default source directory is the expected path")
+  @Test
+  void defaultSourceDirectoryIsTheExpectedPath(@TempDir Path tempDir) {
+    // Given
+    var mockCurrentProject = mock(MavenProject.class);
+    when(mockCurrentProject.getBasedir())
+        .thenReturn(tempDir.toFile());
+
+    var mockMavenSession = mock(MavenSession.class);
+    when(mockMavenSession.getCurrentProject())
+        .thenReturn(mockCurrentProject);
+
+    // Then
+    assertThat(getInstance().defaultSourceDirectory(mockMavenSession))
+        .isEqualTo(expectedDefaultSourceDirectory(mockMavenSession));
+  }
+
+  @DisplayName("the default output directory is the expected path")
+  @Test
+  void defaultOutputDirectoryIsTheExpectedPath(@TempDir Path tempDir) {
+    // Given
+    var mockBuild = mock(Build.class);
+    when(mockBuild.getDirectory())
+        .thenReturn(tempDir.toString());
+
+    var mockCurrentProject = mock(MavenProject.class);
+    when(mockCurrentProject.getBuild())
+        .thenReturn(mockBuild);
+
+    var mockMavenSession = mock(MavenSession.class);
+    when(mockMavenSession.getCurrentProject())
+        .thenReturn(mockCurrentProject);
+
+    // Then
+    assertThat(getInstance().defaultOutputDirectory(mockMavenSession))
+        .isEqualTo(expectedDefaultOutputDirectory(mockMavenSession));
+  }
+}
---
+++ src/test/java/io/github/ascopes/protobufmavenplugin/TestGenerateMojoTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2023 - 2024, Ashley Scopes.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ascopes.protobufmavenplugin;
+
+import io.github.ascopes.protobufmavenplugin.generate.SourceRootRegistrar;
+import java.nio.file.Path;
+import org.apache.maven.execution.MavenSession;
+
+class TestGenerateMojoTest extends AbstractGenerateMojoTestTemplate<TestGenerateMojo> {
+
+  @Override
+  TestGenerateMojo getInstance() {
+    return new TestGenerateMojo();
+  }
+
+  @Override
+  SourceRootRegistrar expectedSourceRootRegistrar() {
+    return SourceRootRegistrar.TEST;
+  }
+
+  @Override
+  Path expectedDefaultSourceDirectory(MavenSession session) {
+    return session.getCurrentProject().getBasedir().toPath()
+        .resolve("src")
+        .resolve("test")
+        .resolve("protobuf");
+  }
+
+  @Override
+  Path expectedDefaultOutputDirectory(MavenSession session) {
+    return Path.of(session.getCurrentProject().getBuild().getDirectory())
+        .resolve("generated-test-sources")
+        .resolve("protobuf");
+  }
+}
