3917455b87d5c0f43e14e9cfc2a977e99bf5a218
Ashley <73482956+ascopes@users.noreply.github.com>
2024-05-06 10:08:30 +0100

    Update license header for close-nexus-repos.sh
    [skip ci]

--- scripts/close-nexus-repos.sh
+++ scripts/close-nexus-repos.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2023, ascopes
+# Copyright (C) 2023 - 2024, ascopes
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
