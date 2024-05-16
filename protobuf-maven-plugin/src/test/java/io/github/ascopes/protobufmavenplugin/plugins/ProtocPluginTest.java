3dd88780138e4a4f6561635dfa983ea58756972b
ascopes <73482956+ascopes@users.noreply.github.com>
2024-05-16 08:11:03 +0100

    Add test case for ProtocPlugin.java

---
+++ protobuf-maven-plugin/src/test/java/io/github/ascopes/protobufmavenplugin/plugins/ProtocPluginTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2023 - 2024, Ashley Scopes.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ascopes.protobufmavenplugin.plugins;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("ProtocPlugin tests")
+class ProtocPluginTest {
+
+  @DisplayName("The default value for isSkip is the expected value")
+  @Test
+  void theDefaultValueForIsSkipIsTheExpectedValue() {
+    // Given
+    var obj = mock(ProtocPlugin.class);
+    when(obj.isSkip()).thenCallRealMethod();
+
+    // Then
+    assertThat(obj.isSkip()).isFalse();
+  }
+}
