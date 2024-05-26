33f31a206540bd888723978e859172184667e3ce
ascopes <73482956+ascopes@users.noreply.github.com>
2024-05-26 10:52:42 +0100

    Add script to run site locally

---
+++ scripts/site-local.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+###
+### Runs a web server hosting the generated site locally.
+###
+
+set -o errexit
+set -o xtrace
+
+cd "$(git rev-parse --show-toplevel)/protobuf-maven-plugin/target/site"
+python3 -m http.server -b 127.0.0.1 8080 &
+sleep 1
+python -m webbrowser -t http://127.0.0.1:8080
+wait < <(jobs -p)
