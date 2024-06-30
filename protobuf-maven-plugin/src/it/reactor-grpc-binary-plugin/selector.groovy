d6e459fadeb223c8419c809a72da777fa117a609
ascopes <73482956+ascopes@users.noreply.github.com>
2024-06-30 12:29:07 +0100

    Skip ITs on Android if they cannot invoke the binaries correctly

--- protobuf-maven-plugin/src/it/reactor-grpc-binary-plugin/selector.groovy
+++ protobuf-maven-plugin/src/it/reactor-grpc-binary-plugin/selector.groovy
@@ -13,4 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-return ["x86_64", "amd64", "aarch64"].contains(System.getProperty("os.arch"))
+
+if (!["x86_64", "amd64", "aarch64"].contains(System.getProperty("os.arch"))) {
+  println("Skipping this test case as the system is not AMD64 or AARCH64")
+  return false
+} else if ("uname -o".execute().text =~ /Android.*/) {
+  println("Skipping test as it uses incompatible binaries for Android")
+  return false
+} else {
+  return true
+}
