803eb51bfd4417acd2b9eb56331d5a776a7cbe02
ascopes <73482956+ascopes@users.noreply.github.com>
2024-07-08 08:12:36 +0100

    Fix site-local.sh not cleaning Python up in the background

--- scripts/site-local.sh
+++ scripts/site-local.sh
@@ -5,10 +5,12 @@
 ### Author: Ashley Scopes
 ###
 set -o errexit
-set -o xtrace
+[[ -n ${DEBUG+undef} ]] && set -o xtrace
 
 cd "$(git rev-parse --show-toplevel)/protobuf-maven-plugin/target/site"
 python3 -m http.server -b 127.0.0.1 8080 &
+readonly pid=$!
+trap 'kill -SIGTERM $pid' EXIT INT TERM
 sleep 1
 python -m webbrowser -t http://127.0.0.1:8080
 wait < <(jobs -p)
