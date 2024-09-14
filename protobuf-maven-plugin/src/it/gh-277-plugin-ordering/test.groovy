e80f80c8d24d030c47b82b7ab212864f1d312076
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2024-09-14 15:40:59 +0100

    Tidy up imports

--- protobuf-maven-plugin/src/it/gh-277-plugin-ordering/test.groovy
+++ protobuf-maven-plugin/src/it/gh-277-plugin-ordering/test.groovy
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import java.nio.file.Files
 import java.nio.file.Path
-import java.util.NoSuchElementException
 
 import static org.assertj.core.api.Assertions.assertThat
 import static org.assertj.core.api.Assertions.fail
--- protobuf-maven-plugin/src/main/java/io/github/ascopes/protobufmavenplugin/plugins/JvmPluginResolver.java
+++ protobuf-maven-plugin/src/main/java/io/github/ascopes/protobufmavenplugin/plugins/JvmPluginResolver.java
@@ -33,7 +33,6 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
