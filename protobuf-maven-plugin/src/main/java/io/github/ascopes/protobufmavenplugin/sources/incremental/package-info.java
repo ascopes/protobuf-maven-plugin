6f5bf10cb09bc05ee35162e8f5e40d74ed4e42e1
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2024-11-10 19:08:27 +0000

    Add missing package-info.java

---
+++ protobuf-maven-plugin/src/main/java/io/github/ascopes/protobufmavenplugin/sources/incremental/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2023 - 2024, Ashley Scopes.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Components to facilitate incremental code generation, which aims to reduce build times for
+ * large codebases by allowing the {@code maven-compiler-plugin} to incrementally generate classes
+ * from Java sources.
+ */
+package io.github.ascopes.protobufmavenplugin.sources.incremental;
