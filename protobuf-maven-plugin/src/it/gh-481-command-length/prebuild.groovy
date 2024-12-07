03be80e1e33eec4bfced80e28a9afa750c6a6ae5
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2024-12-07 12:43:35 +0000

    GH-481: Create test to reproduce large builds

---
+++ protobuf-maven-plugin/src/it/gh-481-command-length/prebuild.groovy
@@ -0,0 +1,146 @@
+/*
+ * Copyright (C) 2023 - 2024, Ashley Scopes.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import groovy.transform.Field
+
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.concurrent.Executors
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Future
+
+static final @Field String ALPHA = "abcdefghijklmnopqrstuvwxyz"
+static final @Field String ALPHA_NUMERIC = ALPHA + "0123456789"
+static final @Field String ALPHA_NUMERIC_UNDERSCORE = ALPHA_NUMERIC + "_"
+static final @Field List<String> PROTO_TYPES = [
+    "double", "float", "int32", "int64", "uint32", "uint64", "sint32", "sint64",
+    "fixed32", "fixed64", "sfixed32", "sfixed64", "bool", "string", "bytes",
+]
+
+@Field Random rng = new Random()
+
+int randomInt(int min, int max) {
+  return min + rng.nextInt(max - min)
+}
+
+char randomChar(String dictionary) {
+  return dictionary[rng.nextInt(dictionary.length())]
+}
+
+void addRandomMessageName(Appendable sb) {
+  int length = randomInt(5, 10)
+  sb.append(Character.toUpperCase(randomChar(ALPHA)))
+  for (int i = 1; i < length; ++i) {
+    sb.append(randomChar(ALPHA_NUMERIC))
+  }
+}
+
+void addRandomIdentifier(Appendable sb) {
+  int length = randomInt(5, 10)
+  sb.append(randomChar(ALPHA))
+  for (int i = 1; i < length; ++i) {
+    sb.append(randomChar(ALPHA_NUMERIC_UNDERSCORE))
+  }
+}
+
+void addRandomMessage(Appendable sb) {
+  sb.append("\nmessage ")
+  addRandomMessageName(sb)
+  sb.append(" {\n")
+  int fieldCount = randomInt(1, 5)
+  for (int i = 1; i <= fieldCount; ++i) {
+    sb.append("  ")
+        .append(PROTO_TYPES[rng.nextInt(PROTO_TYPES.size())])
+        .append(" ")
+    addRandomIdentifier(sb)
+    sb.append(" = ")
+        .append(i.toString())
+        .append(";\n")
+  }
+  sb.append("}\n")
+}
+
+void addRandomProtoFile(int index, Appendable sb) {
+  // File header
+  sb.append('syntax = "proto3";\n')
+      .append('option java_multiple_files = true;\n')
+      .append('option java_package = "')
+
+  sb.append("javapackage_").append(index.toString()).append(".")
+  int javaPackageLength = randomInt(1, 5)
+  addRandomIdentifier(sb)
+  for (int i = 1; i < javaPackageLength; ++i) {
+    sb.append(".")
+    addRandomIdentifier(sb)
+  }
+
+  sb.append('";\n\n')
+    .append("package ")
+
+  sb.append("protopackage_").append(index.toString()).append(".")
+  int protoPackageLength = randomInt(1, 5)
+  addRandomIdentifier(sb)
+  for (int i = 1; i < protoPackageLength; ++i) {
+    sb.append(".")
+    addRandomIdentifier(sb)
+  }
+  sb.append(';\n')
+
+  // File contents, one to two messages each.
+  int messageCount = randomInt(1, 2)
+  for (int i = 0; i < messageCount; ++i) {
+    addRandomMessage(sb)
+  }
+}
+
+String randomFileName(int index) {
+  StringBuilder fileName = new StringBuilder()
+  // Ensure the file names remain unique.
+  fileName.append(Character.toUpperCase(randomChar(ALPHA)))
+  int length = randomInt(1, 10)
+  for (int i = 1; i < length; ++i) {
+    fileName.append(randomChar(ALPHA_NUMERIC))
+  }
+  return fileName.append("_${index}.proto").toString()
+}
+
+@SuppressWarnings('GroovyAssignabilityCheck')
+Future<Void> generateRandomFile(int index, Path baseDir, ExecutorService executor) {
+  executor.submit {
+    Path path = baseDir.resolve(randomFileName(index))
+    try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+      addRandomProtoFile(index, writer)
+    }
+    return null
+  }
+}
+
+void generateRandomFiles(int count, ExecutorService executor) {
+  println("Generating ${count} random proto files files for this test...")
+  Path baseDir = basedir.toPath().resolve("src").resolve("main").resolve("protobuf")
+  Files.createDirectories(baseDir)
+  List<Future<Void>> futures = []
+  for (int i = 0; i < count; ++i) {
+    futures.add(generateRandomFile(i, baseDir, executor))
+  }
+  for (Future<Void> future : futures) {
+    future.get()
+  }
+}
+
+ExecutorService executor = Executors.newFixedThreadPool(30)
+generateRandomFiles(500, executor)
+executor.shutdown()
