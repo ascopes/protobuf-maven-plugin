c65656282cb4dc1107254c361f4b0d1181ec25d0
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2025-01-04 09:56:43 +0000

    Update dependencies to Maven 4

---
+++ protobuf-maven-plugin/src/it/help-goal/selector.groovy
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2023 - 2025, Ashley Scopes.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+println("The help mojo is disabled on Maven 4 builds due to use of deprecated APIs")
+println("This can be re-enabled once maven-plugin-plugin is fixed to address this issue")
+return false
