87fb5b8305e95e5f8e83ff75866d6187fde02eef
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2025-03-23 10:46:56 +0000

    Improve scripts/site-local.sh to support ooening on termux

--- scripts/site-local.sh
+++ scripts/site-local.sh
@@ -5,12 +5,25 @@
 ### Author: Ashley Scopes
 ###
 set -o errexit
+set -o pipefail
 [[ -n ${DEBUG+undef} ]] && set -o xtrace
 
+readonly host=127.0.0.1
+readonly port=9000
+
 cd "$(git rev-parse --show-toplevel)/protobuf-maven-plugin/target/site"
-python3 -m http.server -b 127.0.0.1 9000 &
+python3 -m http.server -b "${host}" "${port}" &
 readonly pid=$!
 trap 'kill -SIGTERM $pid' EXIT INT TERM
-sleep 1
-python -m webbrowser -t http://127.0.0.1:9000
-wait < <(jobs -p)
+
+until curl --connect-timeout 1 --fail --silent -I "http://${host}:${port}" | head -n 1; do
+  sleep 0.5
+done
+
+if command -v termux-open-url 2>/dev/null; then
+  termux-open-url "http://${host}:${port}"
+else
+  python3 -m webbrowser -t "http://${host}:${port}"
+fi
+
+wait
