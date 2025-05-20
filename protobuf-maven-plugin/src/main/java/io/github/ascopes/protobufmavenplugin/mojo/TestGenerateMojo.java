10f95a901611a96a338a6708628c6900d7adbdab
Fae Hutter <haku@users.noreply.github.com>
2025-05-20 20:02:27 +0100

    Allow running in offline mode (#670)
    See https://github.com/ascopes/protobuf-maven-plugin/issues/668 for
    background and reasoning.

--- protobuf-maven-plugin/src/main/java/io/github/ascopes/protobufmavenplugin/mojo/TestGenerateMojo.java
+++ protobuf-maven-plugin/src/main/java/io/github/ascopes/protobufmavenplugin/mojo/TestGenerateMojo.java
@@ -50,7 +50,7 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
     defaultPhase = LifecyclePhase.GENERATE_TEST_SOURCES,
     requiresDependencyCollection = ResolutionScope.TEST,
     requiresDependencyResolution = ResolutionScope.TEST,
-    requiresOnline = true,
+    requiresOnline = false,
     threadSafe = true
 )
 public final class TestGenerateMojo extends AbstractGenerateMojo {
