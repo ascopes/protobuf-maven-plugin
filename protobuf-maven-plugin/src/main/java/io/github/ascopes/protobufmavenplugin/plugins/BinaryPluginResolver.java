e6e2da691010434d3cdf7681bd2dd5f876fe6a5a
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2025-09-14 11:48:48 +0100

    GH-802: Reimplement URL handling by decorating input streams with commons-compress
    This reimplements all the low-level URL handling logic internally to remove
    the SPI workaround that is currently in place, and use a recursive dispatcher
    that has custom URLStreamHandlerFactories associated with it. These
    implementations can delegate to archivers and compressors in Apache
    commons-compress to provide support for protocols such as 'tar:gz:https://...'.
    
    This effectively allows users to utilise arbitrary tarballs as the source of
    their plugins now rather than just JARs.
    
    Current implementation provides:
    - Archivers for 'jar', 'war', 'ear', 'zip', 'tar'
    - Compressors for 'gzip', 'bz2'
    
    We can add further integrations such as 'xz', 'cpio', 'ar', 'ar2', 'lzma',
    '7z', 'deflate', 'z', 'zstd', etc as needed by appending to the list
    of providers in the new UrlFactory.
    
    This has currently only been tested using tar.gz to prove that nested
    retrieval works as expected. Other implementations will still need
    unit tests to be added.
    
    In addition, the tests for UriResourceFetcher are now invalid and need
    a full rewrite to handle delegating to this new mechanism under the
    hood.

--- protobuf-maven-plugin/src/main/java/io/github/ascopes/protobufmavenplugin/plugins/BinaryPluginResolver.java
+++ protobuf-maven-plugin/src/main/java/io/github/ascopes/protobufmavenplugin/plugins/BinaryPluginResolver.java
@@ -21,7 +21,7 @@ import io.github.ascopes.protobufmavenplugin.dependencies.MavenArtifactPathResol
 import io.github.ascopes.protobufmavenplugin.dependencies.PlatformClassifierFactory;
 import io.github.ascopes.protobufmavenplugin.digests.Digest;
 import io.github.ascopes.protobufmavenplugin.fs.FileUtils;
-import io.github.ascopes.protobufmavenplugin.fs.UriResourceFetcher;
+import io.github.ascopes.protobufmavenplugin.urls.UriResourceFetcher;
 import io.github.ascopes.protobufmavenplugin.utils.ResolutionException;
 import io.github.ascopes.protobufmavenplugin.utils.SystemPathBinaryResolver;
 import java.io.BufferedInputStream;
