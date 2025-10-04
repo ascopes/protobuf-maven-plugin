15731e9e81acfe6ff5c7da62b6c9ab088697ea91
Ash <73482956+ascopes@users.noreply.github.com>
2025-10-04 09:10:06 +0100

    Fix formatting in dedent.sh
    [skip ci]

--- scripts/dedent.sh
+++ scripts/dedent.sh
@@ -2,11 +2,18 @@
 ###
 ### Intelligently dedents input from stdin.
 ###
+### Useful for quickly reformatting example snippets when writing documentation.
+###
 ### Author: Ashley Scopes
 ###
 set -o errexit
 set -o nounset
 [[ -v DEBUG ]] && set -o xtrace
+
+if ! command -v expand >/dev/null; then
+  printf 'ERROR: expand not found on $PATH.\n' >&2
+  exit 1
+fi
 
 min_indent=
 lines=()
@@ -15,15 +22,15 @@ while IFS=$'\n' read -r line; do
   line=$(expand -it2 <<< "${line}")
 
   # Sniff indents at the start of lines.
-  [[ "${line}" =~ ^( *)(.*)$ ]]
+  [[ ${line} =~ ^( *)(.*)$ ]]
 
   # Don't bother sniffing whitespace indents on blank lines or
   # empty lines as it produces unintentional results.
   if [[ -n ${BASH_REMATCH[2]} ]]; then
-   indent=${#BASH_REMATCH[1]}
-   # Find the next smallest indent if this line has a smaller indent
-   # than previous lines.
-   if [[ -z "${min_indent}" ]] || ((indent < min_indent)); then
+    indent=${#BASH_REMATCH[1]}
+    # Find the next smallest indent if this line has a smaller indent
+    # than previous lines.
+    if [[ -z ${min_indent} ]] || ((indent < min_indent)); then
       min_indent=${indent}
     fi
   fi
@@ -33,6 +40,5 @@ done
 
 # Slice the minimum indent off each line.
 for line in "${lines[@]}"; do
-  echo "${line:${min_indent}:${#line}}"
+  printf "%s\n" "${line:${min_indent}:${#line}}"
 done
-
