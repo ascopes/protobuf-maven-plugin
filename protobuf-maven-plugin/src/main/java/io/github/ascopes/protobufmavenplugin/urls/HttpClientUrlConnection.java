e5383271352e34d13418d6670548bb4c497e729e
Ilya-Kanstanchuk <ilyakonst95@gmail.com>
2025-10-10 14:15:06 +0200

    GH-824: Add HttpClientUrlConnection for handling HTTP/HTTPS requests via HttpClient

---
+++ protobuf-maven-plugin/src/main/java/io/github/ascopes/protobufmavenplugin/urls/HttpClientUrlConnection.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2023 - 2025, Ashley Scopes.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.ascopes.protobufmavenplugin.urls;
+
+import io.github.ascopes.protobufmavenplugin.utils.HttpRequestException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * URL connection for HTTP and HTTPS requests
+ * that wraps HttpClient
+ *
+ *
+ * @author Ilja Kanstanczuk
+ * @since 3.10.1
+ */
+public class HttpClientUrlConnection extends URLConnection {
+
+  private final HttpClient client;
+  private final HttpRequest request;
+  private HttpResponse<String> response;
+
+  public HttpClientUrlConnection(URL url) throws URISyntaxException {
+    super(url);
+    String protocol = url.getProtocol();
+    this.client = HttpClient
+        .newBuilder()
+        .version(protocol.equalsIgnoreCase("http") ? Version.HTTP_1_1: Version.HTTP_2)
+        .build();
+    this.request = HttpRequest
+        .newBuilder()
+        .uri(url.toURI())
+        .GET()
+        .build();
+  }
+
+  @Override
+  public void connect() throws IOException {
+    if (connected) return;
+    try {
+      response = client.send(request, BodyHandlers.ofString());
+      this.connected = true;
+
+      if (response != null && response.statusCode() >= 400){
+        throw HttpRequestException.fromHttpResponse(response, null);
+      }
+    }catch (IOException | InterruptedException e){
+      if (response != null){
+        throw HttpRequestException.fromHttpResponse(response, e);
+      }
+      throw new IOException("Failed to fetch " + url, e);
+    }
+  }
+
+  @Override
+  public InputStream getInputStream() throws IOException {
+    if (!connected) connect();
+    return new ByteArrayInputStream(response.body().getBytes(StandardCharsets.UTF_8));
+  }
+}
