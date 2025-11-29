a12dbea4da3873c44cc64fc57cd6ffa363be52b7
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2025-11-29 11:01:03 +0000

    GH-877: Create initial draft of protoc distribution types

---
+++ protobuf-maven-plugin/src/main/java/io/github/ascopes/protobufmavenplugin/protoc/dists/PathProtocDistribution.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2023 - 2025, Ashley Scopes.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.ascopes.protobufmavenplugin.protoc.dists;
+
+import io.github.ascopes.protobufmavenplugin.digests.Digest;
+import java.util.Optional;
+import org.immutables.value.Value.Modifiable;
+import org.jspecify.annotations.Nullable;
+
+
+/**
+ * Implementation independent descriptor for a {@code protoc}
+ * distribution that is on the system {@code PATH}.
+ *
+ * @author Ashley Scopes
+ * @since TBC
+ */
+@Modifiable
+public abstract non-sealed class PathProtocDistribution
+    implements ProtocDistribution {
+
+  public String getName() {
+    return "protoc";
+  }
+
+  public @Nullable Digest getDigest() {
+    return null;
+  }
+
+  @Override
+  public String toString() {
+    var sb = new StringBuilder()
+        .append(getName());
+
+    Optional.ofNullable(getDigest())
+        .map(Digest::toString)
+        .map("#digest="::concat)
+        .ifPresent(sb::append);
+
+    return sb.toString();
+  }
+}
