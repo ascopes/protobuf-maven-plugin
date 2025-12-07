00fca9ab82e61761acf516cb35a9ee75bb428704
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2025-12-07 09:11:20 +0000

    Remove 'sourcePaths' alias for 'sourceDirectories' parameter

--- protobuf-maven-plugin/src/main/java/io/github/ascopes/protobufmavenplugin/mojo/AbstractGenerateMojo.java
+++ protobuf-maven-plugin/src/main/java/io/github/ascopes/protobufmavenplugin/mojo/AbstractGenerateMojo.java
@@ -756,7 +756,7 @@ public abstract class AbstractGenerateMojo extends AbstractMojo {
    *
    * @since 0.0.1
    */
-  @Parameter(alias = "sourcePaths")
+  @Parameter
   @Nullable List<Path> sourceDirectories;
 
   /**
