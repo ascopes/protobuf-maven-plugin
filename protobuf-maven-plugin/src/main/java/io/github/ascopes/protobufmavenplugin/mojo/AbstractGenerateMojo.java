36b165de4984621aab23512b6de03ec9f48eab1b
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2026-02-02 06:45:05 +0000

    Remove deprecation message added from d497214

--- protobuf-maven-plugin/src/main/java/io/github/ascopes/protobufmavenplugin/mojo/AbstractGenerateMojo.java
+++ protobuf-maven-plugin/src/main/java/io/github/ascopes/protobufmavenplugin/mojo/AbstractGenerateMojo.java
@@ -819,11 +819,6 @@ public abstract class AbstractGenerateMojo extends AbstractMojo {
       return;
     }
 
-    reportDeprecatedProtocPluginsArgument(binaryMavenPlugins, "binaryMavenPlugin", "binary-maven");
-    reportDeprecatedProtocPluginsArgument(binaryPathPlugins, "binaryPathPlugin", "path");
-    reportDeprecatedProtocPluginsArgument(binaryUrlPlugins, "binaryUrlPlugin", "url");
-    reportDeprecatedProtocPluginsArgument(jvmMavenPlugins, "jvmMavenPlugin", "jvm-maven");
-
     var enabledLanguages = Language.setBuilder()
         .addIf(javaEnabled, Language.JAVA)
         .addIf(kotlinEnabled, Language.KOTLIN)
@@ -938,41 +933,5 @@ public abstract class AbstractGenerateMojo extends AbstractMojo {
 
   private <K, V> Map<K, V> nonNullMap(@Nullable Map<K, V> map) {
     return requireNonNullElseGet(map, Map::of);
-  }
-
-  private void reportDeprecatedProtocPluginsArgument(
-      @Nullable Collection<?> arg,
-      String singularName,
-      String replacementKind
-  ) {
-    if (arg == null || arg.isEmpty()) {
-      return;
-    }
-
-    var before = String.join(
-        "\n",
-        "  <" + singularName + "s>",
-        "    <" + singularName + ">",
-        "      ...",
-        "    </" + singularName + ">",
-        "  </" + singularName + "s>"
-    );
-
-    var after = String.join(
-        "\n",
-        "  <protocPlugins>",
-        "    <protocPlugin kind=\"" + replacementKind + "\">",
-        "      ...",
-        "    </protocPlugin>",
-        "  </protocPlugins>"
-    );
-
-    log.warn(
-        "[DEPRECATED] The \"{}s\" attribute is deprecated for removal in v5.0.0. Please "
-            + "replace\n{}\n...with...\n{}\n moving forwards.",
-        singularName,
-        before,
-        after
-    );
   }
 }
